a8adc657c8c5e7933359d6ce85eed5e9e03b6860
Lauren <laurengilbert1997@gmail.com>
2018-07-25 23:43:21 +1200

    Added extra pop up

--- js/scripts.js
+++ js/scripts.js
@@ -93,6 +93,7 @@ function filterByNumberOfPeople(unfilteredVehicles, numberOfPeople) {
     return filteredVehicle;
 }
 
+let noVehicles = `<h2>No available Vehicles :(</h2>`;
 function filterByNumberOfDays(unfilteredVehicles, numberOfDays) {
     var filteredVehicle = {};
 
@@ -113,6 +114,20 @@ quoteButton.click(function(event){
 
     var vehiclesToDisplay = filterByNumberOfPeople(vehicles, quotePeopleDropDown);
     vehiclesToDisplay = filterByNumberOfDays(vehiclesToDisplay, quoteDaysDropDown);
+
+    let tryAgainMessage = `<h3 class="col text-center">Try Again</h3>`;
+    
+
+    if (quotePeopleDropDown == "Select People" ) {
+        $vehicleList.empty();
+        $vehicleList.append(tryAgainMessage);
+        return;
+    } else if (quoteDaysDropDown == "Select Days" ) {
+        $vehicleList.empty();
+        $vehicleList.append(tryAgainMessage);
+        return;
+    }
+
     $vehicleList.empty();
 
 
@@ -128,7 +143,7 @@ quoteButton.click(function(event){
                                     </div>
                                 </div>
                             </div>`;
-        let tryAgainMessage = `<h2>No available Vehicles :(</h2>`;
+        
         
         $vehicleList.append(vehicleItem);
        /*if (vehicles[key].minimumPeople <= quotePeopleDropDown && vehicles[key].maximumPeople >= quotePeopleDropDown && vehicles[key].minimumDays <= quoteDaysDropDown && vehicles[key].maximumDays >= quoteDaysDropDown) {  
@@ -256,215 +271,6 @@ $(".vehicle-button").click(function(event) {
         }    
     });
 })
-
-/*
-vespaQuoteButton.click(function(event) {
-    let vespabuttonAttr = vespaQuoteButton.attr("name");
-    vehicleBody.empty();
-    selectedDaySpan.empty();
-    let vehicleQuoteItem = `<div class="col-lg-6">
-                                <img class="card-img-top" src="img/${vehicles[vespabuttonAttr].image}" alt="Card image cap">
-                            </div>
-                            <div class="col-lg-6 text-left">
-                                <h5 class="card-title">${vehicles[vespabuttonAttr].year} ${vehicles[vespabuttonAttr].name}</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text</p>
-                                <p class="price">$${vehicles[vespabuttonAttr].price} per day</p>
-                                <p class="price">Maximum ${vehicles[vespabuttonAttr].maximumPeople} people</p>
-                            </div>`;
-                        
-    let vespaMaxDaysItem = `<option selected value="0">Select days</option>
-                        <option value="1" name="1">1</option>
-                        <option value="2" name="2">2</option>
-                        <option value="3" name="3">3</option>
-                        <option value="4" name="4">4</option>
-                        <option value="5" name="5">5</option>`;
-
-    Object.keys(vehicles).forEach(function(key) {
-        if (vespabuttonAttr in vehicles) {
-            vehicleBody.empty();
-            daysVehicleSelect.empty();
-            deleteMe.show();
-            selectedDayP.hide();
-            distanceVehicleSelect.val(0);
-            finalSelectedPrice.empty();
-            selectedDistancePrice.empty();
-            selectedDaySpan.empty();
-            vehicleBody.append(vehicleQuoteItem);
-            pricePerDaySpan.html("$" + vehicles[vespabuttonAttr].price.toFixed(2));
-            litresSpan.html(vehicles[vespabuttonAttr].distance + "L/100km");
-            daysVehicleSelect.append(vespaMaxDaysItem);
-
-            daysVehicleSelect.change(function() {
-                var userSelectedDays = this.value;
-                var vespaDaysPrice = userSelectedDays * vehicles[vespabuttonAttr].price;
-                selectedDaySpan.html("$" + vespaDaysPrice.toFixed(2));
-
-                distanceVehicleSelect.change(function() {
-                    var userSelectedDistance = this.value;
-                    var vespaDistancePrice = userSelectedDistance * vehicles[vespabuttonAttr].distance;
-                    selectedDistancePrice.html("$" + vespaDistancePrice.toFixed(2));
-                    var finalPrice = vespaDaysPrice + vespaDistancePrice;
-                    finalSelectedPrice.html("$" + finalPrice.toFixed(2));
-                });
-            });
-            distanceVehicleSelect.change(function() {
-                var userSelectedDistance = this.value;
-                var vespaDistancePrice = userSelectedDistance * vehicles[vespabuttonAttr].distance;
-                selectedDistancePrice.html("$" + vespaDistancePrice.toFixed(2));
-
-                daysVehicleSelect.change(function() {
-                    var userSelectedDays = this.value;
-                    var vespaDaysPrice = userSelectedDays * vehicles[vespabuttonAttr].price;
-                    selectedDaySpan.html("$" + vespaDaysPrice.toFixed(2));
-                    var finalPrice = vespaDaysPrice + vespaDistancePrice;
-                    finalSelectedPrice.html("$" + finalPrice.toFixed(2));
-                });
-            });
-        }    
-    });
-});*/
-/*
-fordQuoteButton.click(function(event) {
-    let fordbuttonAttr = fordQuoteButton.attr("name");
-    vehicleBody.empty();
-    selectedDaySpan.empty();
-    let vehicleQuoteItem = `<div class="col-lg-6">
-                                <img class="card-img-top" src="img/${vehicles[fordbuttonAttr].image}" alt="Card image cap">
-                            </div>
-                            <div class="col-lg-6 text-left">
-                                <h5 class="card-title">${vehicles[fordbuttonAttr].year} ${vehicles[fordbuttonAttr].name}</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text</p>
-                                <p class="price">$${vehicles[fordbuttonAttr].price} per day</p>
-                                <p class="price">Maximum 1 person</p>
-                            </div>`;
-                        
-    let fordMaxDaysItem = `<option selected value="0">Select days</option>
-                        <option value="1" name="1">1</option>
-                        <option value="2" name="2">2</option>
-                        <option value="3" name="3">3</option>
-                        <option value="4" name="4">4</option>
-                        <option value="5" name="5">5</option>
-                        <option value="6" name="6">6</option>
-                        <option value="7" name="7">7</option>
-                        <option value="8" name="8">8</option>
-                        <option value="9" name="9">9</option>
-                        <option value="10" name="10">10</option>`;
-
-    Object.keys(vehicles).forEach(function(key) {
-        if (fordbuttonAttr in vehicles) {
-            vehicleBody.empty();
-            daysVehicleSelect.empty();
-            deleteMe.show();
-            selectedDayP.hide();
-            distanceVehicleSelect.val(0);
-            finalSelectedPrice.empty();
-            selectedDistancePrice.empty();
-            selectedDaySpan.empty();
-            vehicleBody.append(vehicleQuoteItem);
-            pricePerDaySpan.html("$" + vehicles[fordbuttonAttr].price.toFixed(2));
-            litresSpan.html(vehicles[fordbuttonAttr].distance + "L/100km");
-            daysVehicleSelect.append(fordMaxDaysItem);
-
-            daysVehicleSelect.change(function() {
-                var userSelectedDays = this.value;
-                var fordDaysPrice = userSelectedDays * vehicles[fordbuttonAttr].price;
-                selectedDaySpan.html("$" + fordDaysPrice.toFixed(2));
-
-                distanceVehicleSelect.change(function() {
-                    var userSelectedDistance = this.value;
-                    var fordDistancePrice = userSelectedDistance * vehicles[fordbuttonAttr].distance;
-                    selectedDistancePrice.html("$" + fordDistancePrice.toFixed(2));
-                    var finalPrice = fordDaysPrice + fordDistancePrice;
-                    finalSelectedPrice.html("$" + finalPrice.toFixed(2));
-                });
-            });
-            distanceVehicleSelect.change(function() {
-                var userSelectedDistance = this.value;
-                var fordDistancePrice = userSelectedDistance * vehicles[fordbuttonAttr].distance;
-                selectedDistancePrice.html("$" + fordDistancePrice.toFixed(2));
-
-                daysVehicleSelect.change(function() {
-                    var userSelectedDays = this.value;
-                    var fordDaysPrice = userSelectedDays * vehicles[fordbuttonAttr].price;
-                    selectedDaySpan.html("$" + fordDaysPrice.toFixed(2));
-                    var finalPrice = fordDaysPrice + fordDistancePrice;
-                    finalSelectedPrice.html("$" + finalPrice.toFixed(2));
-                });
-            });
-        }    
-    });
-});
-
-lanciaQuoteButton.click(function(event) {
-    let fordbuttonAttr = lanciaQuoteButton.attr("name");
-    vehicleBody.empty();
-    selectedDaySpan.empty();
-    let vehicleQuoteItem = `<div class="col-lg-6">
-                                <img class="card-img-top" src="img/${vehicles[fordbuttonAttr].image}" alt="Card image cap">
-                            </div>
-                            <div class="col-lg-6 text-left">
-                                <h5 class="card-title">${vehicles[fordbuttonAttr].year} ${vehicles[fordbuttonAttr].name}</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text</p>
-                                <p class="price">$${vehicles[fordbuttonAttr].price} per day</p>
-                                <p class="price">Maximum 1 person</p>
-                            </div>`;
-                        
-    let fordMaxDaysItem = `<option selected value="0">Select days</option>
-                        <option value="1" name="1">1</option>
-                        <option value="2" name="2">2</option>
-                        <option value="3" name="3">3</option>
-                        <option value="4" name="4">4</option>
-                        <option value="5" name="5">5</option>
-                        <option value="6" name="6">6</option>
-                        <option value="7" name="7">7</option>
-                        <option value="8" name="8">8</option>
-                        <option value="9" name="9">9</option>
-                        <option value="10" name="10">10</option>`;
-
-    Object.keys(vehicles).forEach(function(key) {
-        if (fordbuttonAttr in vehicles) {
-            vehicleBody.empty();
-            daysVehicleSelect.empty();
-            deleteMe.show();
-            selectedDayP.hide();
-            distanceVehicleSelect.val(0);
-            finalSelectedPrice.empty();
-            selectedDistancePrice.empty();
-            selectedDaySpan.empty();
-            vehicleBody.append(vehicleQuoteItem);
-            pricePerDaySpan.html("$" + vehicles[fordbuttonAttr].price.toFixed(2));
-            litresSpan.html(vehicles[fordbuttonAttr].distance + "L/100km");
-            daysVehicleSelect.append(fordMaxDaysItem);
-
-            daysVehicleSelect.change(function() {
-                var userSelectedDays = this.value;
-                var fordDaysPrice = userSelectedDays * vehicles[fordbuttonAttr].price;
-                selectedDaySpan.html("$" + fordDaysPrice.toFixed(2));
-
-                distanceVehicleSelect.change(function() {
-                    var userSelectedDistance = this.value;
-                    var fordDistancePrice = userSelectedDistance * vehicles[fordbuttonAttr].distance;
-                    selectedDistancePrice.html("$" + fordDistancePrice.toFixed(2));
-                    var finalPrice = fordDaysPrice + fordDistancePrice;
-                    finalSelectedPrice.html("$" + finalPrice.toFixed(2));
-                });
-            });
-            distanceVehicleSelect.change(function() {
-                var userSelectedDistance = this.value;
-                var fordDistancePrice = userSelectedDistance * vehicles[fordbuttonAttr].distance;
-                selectedDistancePrice.html("$" + fordDistancePrice.toFixed(2));
-
-                daysVehicleSelect.change(function() {
-                    var userSelectedDays = this.value;
-                    var fordDaysPrice = userSelectedDays * vehicles[fordbuttonAttr].price;
-                    selectedDaySpan.html("$" + fordDaysPrice.toFixed(2));
-                    var finalPrice = fordDaysPrice + fordDistancePrice;
-                    finalSelectedPrice.html("$" + finalPrice.toFixed(2));
-                });
-            });
-        }    
-    });
-});*/
 
 function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
