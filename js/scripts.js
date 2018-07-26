01ba8ac3285dbe72b7a55c9ac1d225df168a613b
Lauren <laurengilbert1997@gmail.com>
2018-07-27 10:04:00 +1200

    Updated comment

--- js/scripts.js
+++ js/scripts.js
@@ -333,7 +333,7 @@ $(".vehicle-button").click(function(event) {
     });
 })
 
-//Validates Email in the contact section
+//Validates Email in the contact section taken from stackoverflow
 function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
