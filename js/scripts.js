1b4015f90d730c7be4afcaa9286535fefbc2ecc3
Lauren <laurengilbert1997@gmail.com>
2018-07-26 13:48:51 +1200

    Added comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-let $vehicleList = $("#show-vehicles");
-
 let vehicles = {
     "vespa" : {
         "name" :"Vespa",
@@ -62,6 +60,8 @@ let vehicles = {
     },
 }
 
+let $vehicleList = $("#show-vehicles");
+
 let vehicleBody = $(".vehicle-body");
 let pricePerDaySpan = $("#day-price");
 let selectedDaySpan = $("#selected-day-price");
@@ -76,7 +76,8 @@ let selectedDayP = $("#selected-day-p");
 let deleteMe = $("#delete-me");
 
 $(document).ready(function() {
-    $('#logo, .nav-link').click(function(e){
+    // Scroll to link
+    $('#logo, .nav-link').click(function(e) {
         e.preventDefault();
         var target = $(this).attr('href');
         var target_div = $(target);
@@ -115,23 +116,27 @@ function filterByNumberOfDays(unfilteredVehicles, numberOfDays) {
 }
 
 // Quote selection and display
-quoteButton.click(function(event){
-    //
+quoteButton.click(function(event) {
+    // Gets what is selected in the drop downs
     let quotePeopleDropDown = $("#people-select option:selected").text();
     let quoteDaysDropDown = $("#days-select option:selected").text();
 
+    // Runs filtering days and people
     var vehiclesToDisplay = filterByNumberOfPeople(vehicles, quotePeopleDropDown);
     vehiclesToDisplay = filterByNumberOfDays(vehiclesToDisplay, quoteDaysDropDown);
 
+    // Error messages if an option isn't selected or no vehicles available
     let tryAgainMessage = `<h3 class="col text-center">Try Again</h3>`;
     let noVehicles = `<h3 class="col text-center">No available Vehicles :(</h3>`;
     
+    // Displays error message if no available vehicles
     if (Object.keys(vehiclesToDisplay).length === 0) {
         $vehicleList.empty();
         $vehicleList.append(noVehicles);
         return;
     }
 
+    // Displays error message if nothing is selected in drop downs
     if (quotePeopleDropDown == "Select People" ) {
         $vehicleList.empty();
         $vehicleList.append(tryAgainMessage);
@@ -144,8 +149,10 @@ quoteButton.click(function(event){
 
     $vehicleList.empty();
 
-
+    // Goes through Vehicle JSON 
     Object.keys(vehiclesToDisplay).forEach(function (key) {
+
+        // Displays whats is inside each vehicle card
         let vehicleItem = `<div class="col-sm-12 col-md-6 col-lg-4">
                                     <div class="card-modal text-center">
                                         <img class="card-img-top" src="img/${vehicles[key].image}" alt="Card image cap">
@@ -158,52 +165,62 @@ quoteButton.click(function(event){
                                 </div>
                             </div>`;
         
-        
         $vehicleList.append(vehicleItem);
 
-        let  VehicleQuoteButton= $("#vespa-quote-button");
+        // When quote card button is clicked
         $(".vehicle-select-button").click(function(event) {
-            let vespaVehicleQuoteButtonAttr = $(event.target).attr("data-key");
+
+            // Gets data-key of the button 
+            let VehicleQuoteButtonAttr = $(event.target).attr("data-key");
+            
+            // Contents of Card
+            let vehicleQuoteItem = `<div class="col-lg-6">
+                                        <img class="card-img-top" src="img/${vehicles[VehicleQuoteButtonAttr].image}" alt="Card image cap">
+                                    </div>
+                                    <div class="col-lg-6 text-left">
+                                        <h5 class="card-title">${vehicles[VehicleQuoteButtonAttr].year} ${vehicles[VehicleQuoteButtonAttr].name}</h5>
+                                        <p class="card-text">${vehicles[VehicleQuoteButtonAttr].description}</p>
+                                        <p class="price">$${vehicles[VehicleQuoteButtonAttr].price} per day</p>
+                                        <p class="price">Maximum 1 person</p>
+                                    </div>`;
+
+            // Inside quote - displays what day they selected when choosing a quote
+            let dayP = `Selected days:
+                        <span class="price-span" id="user-selected-day-price">${quoteDaysDropDown}</span>`;
+
+            //empties previous contents inside card and adds content
             selectedDaySpan.empty();
             vehicleBody.empty();
+            vehicleBody.empty();
+            selectedDayP.show();
+            selectedDayP.empty();
+            vehicleBody.append(vehicleQuoteItem);
+            deleteMe.hide();
+            distanceVehicleSelect.val(0);
+            finalSelectedPrice.empty();
+            selectedDistancePrice.empty();
+
+            // Displays price per day and litres per 100km
+            pricePerDaySpan.html("$" + vehicles[VehicleQuoteButtonAttr].price.toFixed(2));
+            litresSpan.html(vehicles[VehicleQuoteButtonAttr].distance + "L/100km");
+            selectedDayP.append(dayP);
             
-            let vehicleQuoteItem = `<div class="col-lg-6">
-                        <img class="card-img-top" src="img/${vehicles[vespaVehicleQuoteButtonAttr].image}" alt="Card image cap">
-                    </div>
-                    <div class="col-lg-6 text-left">
-                        <h5 class="card-title">${vehicles[vespaVehicleQuoteButtonAttr].year} ${vehicles[vespaVehicleQuoteButtonAttr].name}</h5>
-                        <p class="card-text">${vehicles[vespaVehicleQuoteButtonAttr].description}</p>
-                        <p class="price">$${vehicles[vespaVehicleQuoteButtonAttr].price} per day</p>
-                        <p class="price">Maximum 1 person</p>
-                    </div>`;
+            // Works out price per day
+            var DaysPrice = quoteDaysDropDown * vehicles[VehicleQuoteButtonAttr].price;
+            selectedDaySpan.html("$" + DaysPrice.toFixed(2));
 
+            // When user changes distance
+            distanceVehicleSelect.change(function() {
+                var userSelectedDistance = this.value;
 
-                    let dayP = `Selected days:
-        <span class="price-span" id="user-selected-day-price">${quoteDaysDropDown}</span>`;
-                    vehicleBody.empty();
-                    selectedDayP.show();
-                    selectedDayP.empty();
-                    vehicleBody.append(vehicleQuoteItem);
-                    deleteMe.hide();
-                    distanceVehicleSelect.val(0);
-                    finalSelectedPrice.empty();
-                    selectedDistancePrice.empty();
+                // Works out selected distance by litres
+                var DistancePrice = userSelectedDistance * vehicles[VehicleQuoteButtonAttr].distance;
+                selectedDistancePrice.html("$" + DistancePrice.toFixed(2));
 
-
-                    pricePerDaySpan.html("$" + vehicles[vespaVehicleQuoteButtonAttr].price.toFixed(2));
-                    litresSpan.html(vehicles[vespaVehicleQuoteButtonAttr].distance + "L/100km");
-                    selectedDayP.append(dayP);
-                    
-                    var vespaDaysPrice = quoteDaysDropDown * vehicles[vespaVehicleQuoteButtonAttr].price;
-                    selectedDaySpan.html("$" + vespaDaysPrice.toFixed(2));
-
-                    distanceVehicleSelect.change(function() {
-                        var userSelectedDistance = this.value;
-                        var vespaDistancePrice = userSelectedDistance * vehicles[vespaVehicleQuoteButtonAttr].distance;
-                        selectedDistancePrice.html("$" + vespaDistancePrice.toFixed(2));
-                        var finalPrice = vespaDaysPrice + vespaDistancePrice;
-                        finalSelectedPrice.html("$" + finalPrice.toFixed(2));
-                    });
+                // Works out final price (days price plus distance price)
+                var finalPrice = DaysPrice + DistancePrice;
+                finalSelectedPrice.html("$" + finalPrice.toFixed(2));
+            });
         });           
     });
 });
@@ -212,34 +229,43 @@ quoteButton.click(function(event){
 
 // Bottom Vehicle selection
 $(".vehicle-button").click(function(event) {
-    // Targets button 
-    let vespabuttonAttr = $(event.target).attr("data-button");
+    // gets the data-button of button that's clicked
+    let buttonAttr = $(event.target).attr("data-button");
 
     vehicleBody.empty();
     selectedDaySpan.empty();
 
+    // Contents of quote
     let vehicleQuoteItem = `<div class="col-lg-6">
-                                <img class="card-img-top" src="img/${vehicles[vespabuttonAttr].image}" alt="Card image cap">
+                                <img class="card-img-top" src="img/${vehicles[buttonAttr].image}" alt="Card image cap">
                             </div>
                             <div class="col-lg-6 text-left">
-                                <h5 class="card-title">${vehicles[vespabuttonAttr].year} ${vehicles[vespabuttonAttr].name}</h5>
-                                <p class="card-text">${vehicles[vespabuttonAttr].description}</p>
-                                <p class="price">$${vehicles[vespabuttonAttr].price.toFixed(2)} per day</p>
-                                <p class="price">Maximum ${vehicles[vespabuttonAttr].maximumPeople} people</p>
+                                <h5 class="card-title">${vehicles[buttonAttr].year} ${vehicles[buttonAttr].name}</h5>
+                                <p class="card-text">${vehicles[buttonAttr].description}</p>
+                                <p class="price">$${vehicles[buttonAttr].price.toFixed(2)} per day</p>
+                                <p class="price">Maximum ${vehicles[buttonAttr].maximumPeople} people</p>
                             </div>`;
-                        
-    let vespaMaxDaysItem = `<option selected value="0">Select days</option>
-                            <option value="1" name="1">1</option>
-                            <option value="2" name="2">2</option>
-                            <option value="3" name="3">3</option>
-                            <option value="4" name="4">4</option>
-                            <option value="5" name="5">5</option>`;
+      
+    // Contents of Drop down days
+    let MaxDaysItem = `<option selected value="0">Select days</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>`;
 
     // Goes through Vehicles 
     Object.keys(vehicles).forEach(function(key) {
 
-        //if 
-        if (vespabuttonAttr in vehicles) {
+        // if selected button is in vehicles
+        if (buttonAttr in vehicles) {
+
+            // Empties and adds content 
             vehicleBody.empty();
             daysVehicleSelect.empty();
             deleteMe.show();
@@ -249,33 +275,57 @@ $(".vehicle-button").click(function(event) {
             selectedDistancePrice.empty();
             selectedDaySpan.empty();
             vehicleBody.append(vehicleQuoteItem);
-            pricePerDaySpan.html("$" + vehicles[vespabuttonAttr].price.toFixed(2));
-            litresSpan.html(vehicles[vespabuttonAttr].distance + "L/100km");
-            daysVehicleSelect.append(vespaMaxDaysItem);
+            daysVehicleSelect.append(MaxDaysItem);
 
+            //Displays price per day and litres
+            pricePerDaySpan.html("$" + vehicles[buttonAttr].price.toFixed(2));
+            litresSpan.html(vehicles[buttonAttr].distance + "L/100km");
+            
+            /* I had trouble getting the input of the drop down menus early on,
+            so I ended up using the .change() function. The problem with this was that the
+            final price wouldn't update perfectly all the time. So I ended up putting it in 
+            there twice */
+
+            // When days selected has been changed
             daysVehicleSelect.change(function() {
                 var userSelectedDays = this.value;
-                var vespaDaysPrice = userSelectedDays * vehicles[vespabuttonAttr].price;
-                selectedDaySpan.html("$" + vespaDaysPrice.toFixed(2));
 
+                // Works out how much per selected day
+                var DaysPrice = userSelectedDays * vehicles[buttonAttr].price;
+                selectedDaySpan.html("$" + DaysPrice.toFixed(2));
+
+                //When distance has changed
                 distanceVehicleSelect.change(function() {
                     var userSelectedDistance = this.value;
-                    var vespaDistancePrice = userSelectedDistance * vehicles[vespabuttonAttr].distance;
-                    selectedDistancePrice.html("$" + vespaDistancePrice.toFixed(2));
-                    var finalPrice = vespaDaysPrice + vespaDistancePrice;
+
+                    //works out how much per distance
+                    var DistancePrice = userSelectedDistance * vehicles[buttonAttr].distance;
+                    selectedDistancePrice.html("$" + DistancePrice.toFixed(2));
+
+                    // Works out final price
+                    var finalPrice = DaysPrice + DistancePrice;
                     finalSelectedPrice.html("$" + finalPrice.toFixed(2));
                 });
             });
+
+            // When distance has changed
             distanceVehicleSelect.change(function() {
                 var userSelectedDistance = this.value;
-                var vespaDistancePrice = userSelectedDistance * vehicles[vespabuttonAttr].distance;
-                selectedDistancePrice.html("$" + vespaDistancePrice.toFixed(2));
 
+                // Works out how much per distance
+                var DistancePrice = userSelectedDistance * vehicles[buttonAttr].distance;
+                selectedDistancePrice.html("$" + DistancePrice.toFixed(2));
+
+                // When days has been changed
                 daysVehicleSelect.change(function() {
                     var userSelectedDays = this.value;
-                    var vespaDaysPrice = userSelectedDays * vehicles[vespabuttonAttr].price;
-                    selectedDaySpan.html("$" + vespaDaysPrice.toFixed(2));
-                    var finalPrice = vespaDaysPrice + vespaDistancePrice;
+
+                    // Works out how much per selected day
+                    var DaysPrice = userSelectedDays * vehicles[buttonAttr].price;
+                    selectedDaySpan.html("$" + DaysPrice.toFixed(2));
+
+                    // Works out final price
+                    var finalPrice = DaysPrice + DistancePrice;
                     finalSelectedPrice.html("$" + finalPrice.toFixed(2));
                 });
             });
@@ -283,23 +333,24 @@ $(".vehicle-button").click(function(event) {
     });
 })
 
+//Validates Email in the contact section
 function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
-  }
+}
   
-  function validate() {
+    function validate() {
     var $result = $("#alert");
     var email = $("#email").val();
     $result.text("");
   
     if (validateEmail(email)) {
-      $result.text("");
+        $result.text("");
     } else {
-      $result.text("Email is not valid");
-      $result.css("color", "#eb5c23");
+        $result.text("Email is not valid");
+        $result.css("color", "#eb5c23");
     }
     return false;
-  };
-  
-  $("#validate").bind("click", validate);
+};
+
+$("#validate").bind("click", validate);
